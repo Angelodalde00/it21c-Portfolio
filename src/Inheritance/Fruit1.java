f31be625f8c425480f089ff3c3dd917537afdeab
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-17 15:23:59 +0800

    Added inheritance package

---
+++ src/Inheritance/Fruit1.java
@@ -0,0 +1,18 @@
+package Inheritance;
+
+
+public class Fruit1 {
+    
+    String name;
+    
+    Fruit1 (String name){
+        this.name = name;
+    }
+    void kaon(){
+        System.out.println(name + " gi kaon");   
+    }
+    void tulon(){
+        System.out.println(name + " gi tulon");
+    }
+    
+}
---
+++ src/Inheritance/Main.java
@@ -0,0 +1,22 @@
+package Inheritance;
+
+import Inheritance.Santol;
+
+
+
+
+
+public class Main {
+  
+        public static void main(String[] args){
+            
+            Santol mySantol = new Santol("Santol");
+            
+            mySantol.kaon();
+            mySantol.tulon();
+                  
+        }
+        
+        
+        
+}
