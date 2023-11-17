4b242ce8146a65b7e4f3f61c5d9fd9ccae5ff956
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-17 15:24:38 +0800

    Added prajik package

---
+++ src/prajek/Car.java
@@ -0,0 +1,20 @@
+
+package prajek;
+
+//DINAKO UY
+public class Car {
+    String car_name;
+    double car_price;
+    
+    Car(String car_name,double car_price){
+    this.car_name = car_name;
+    this.car_price = car_price;
+    }
+    void showCarName(){
+        System.out.println(car_name +"Sinaw morag ikaw ");
+        
+    }
+    void showCarModel(){
+        System.out.println(car_price + " Mahal pero mas mahal tika yiee <3 ");
+    }
+}
---
+++ src/prajek/Prajek.java
@@ -0,0 +1,15 @@
+
+package prajek;
+
+//Dalde gwapo nawog tao
+
+public class Prajek {
+
+    public static void main(String[] args) {
+        Car car_01 = new Car("Honda ",120.99 );
+        
+        car_01.showCarName();
+        car_01.showCarModel();
+    }
+    
+}
