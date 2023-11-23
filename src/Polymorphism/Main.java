c752a1ae69b60a7b422ee078a3cfd024e11964d9
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-23 16:26:36 +0800

    Polymorphism add

---
+++ src/Polymorphism/Main.java
@@ -0,0 +1,12 @@
+
+package Polymorphism;
+
+
+public class Main {
+    
+        
+        
+        
+        
+    }
+
---
+++ src/Polymorphism/Polymorphism_Subjects.java
@@ -0,0 +1,28 @@
+
+package Polymorphism;
+
+
+public class Polymorphism_Subjects {
+
+    
+    
+    Polymorphism_Subjects(String name){
+        this.name = name;
+    }
+    //Local ni sir wahaha
+    void nagyawyaw(){
+        System.out.println(name + "naminaw sa subject teacher");
+        
+    }
+    void nagsabsab(){
+        System.out.println(name + "nakig tabi si lester");
+    }
+    void kasaba(){
+        System.out.println(name + "Nasuko si teacher");
+    }
+    //Method ni sya chuys
+    public void Sujects(){
+        System.out.println("Naka piso si lester" + name);
+    }
+    
+}
