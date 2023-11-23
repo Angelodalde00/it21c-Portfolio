4c8ba6159f5e7e37a99ccd0f9a4e2ee2315a9e02
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-23 16:23:08 +0800

    First commit prajek

--- src/prajek/Prajek.java
+++ src/prajek/Prajek.java
@@ -6,7 +6,7 @@ package prajek;
 public class Prajek {
 
     public static void main(String[] args) {
-        Car car_01 = new Car("Honda",120.99);
+        Car car_01 = new Car("Honda ",120.99 );
         
         car_01.showCarName();
         car_01.showCarModel();
