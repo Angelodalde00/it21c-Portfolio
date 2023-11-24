9922f6215aa1166409118f2805503af3b8a1ea21
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-24 16:06:48 +0800

    Adding Student

---
+++ src/Student/Student.java
@@ -0,0 +1,21 @@
+package Student;
+
+public class Student {
+    
+    String name;
+    String section;
+    String motto;
+    
+    public String getName(){
+        
+        return name;
+    }
+    public String setSection(){
+        
+        return "IT21C";
+    }
+    public String getMotto(){
+        
+        return "God provides";
+    }
+}
---
+++ src/Student/Student1.java
@@ -0,0 +1,20 @@
+package Student;
+
+
+import Student.Student;
+
+public class Student1 {
+    
+    public static void main(String[]args) {
+        Student student = new Student();
+        
+        student.name = "Lester Angelo O. Dalde";
+        student.section = student.setSection();
+        student.motto = student.getMotto();
+        
+        System.out.println(student.name);
+        System.out.println(student.section);
+        System.out.println(student.motto);
+        
+    }
+}
