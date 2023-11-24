1561a22a8a7f1c269b50fd4c9d82b181907b6073
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-24 16:04:52 +0800

    Adding inheritance

---
+++ src/Inheritance/Santol.java
@@ -0,0 +1,11 @@
+package Inheritance;
+
+public class Santol extends Fruit1{
+    Santol (String name){
+        super(name);
+        
+    }
+    void tukan(){
+        System.out.println(name + "na tuk an");
+    }
+    }
