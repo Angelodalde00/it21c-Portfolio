aa9b1573a2da205923b2bfee26032cff84418824
Angelodalde00 <lesterdalde27@gmail.com>
2023-11-24 16:05:41 +0800

    Adding lamp

---
+++ src/Lamp/Student1.java
@@ -0,0 +1,19 @@
+package Lamp;
+
+public class Student1 {
+
+    String name;
+    int roll_num;
+
+    
+}
+class ans{
+    public static void main(String[] args) {
+    Student1 student = new Student1();
+    student.name="Lester";
+    student.roll_num= 20221287;
+        System.out.println("Hi! my name is "+student.name + " and my id is #"+ student.roll_num);
+        
+    }
+
+}
